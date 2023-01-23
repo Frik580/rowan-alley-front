64c1bbd3eadd7e67b2a121e394b869adc1340d8a
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2023-01-23 20:31:47 +0600

    Раздел Наши партнеры

--- src/components/Main/Partners/Partners.js
+++ src/components/Main/Partners/Partners.js
@@ -1,12 +1,23 @@
 import "./Partners.css";
 import Partner from "../Partner/Partner";
+import imgRus from "../../../images/logo_rus.png";
+import imgGaspi from "../../../images/permgaspi.png";
 
 function Partners() {
   return (
     <div className="partners">
       <h2 className="partners__title">НАШИ ПАРТНЁРЫ</h2>
       <ul className="partners__list">
-    <Partner />
+        <Partner
+          url={"https://myhistorypark.ru/"}
+          src={imgRus}
+          title={"Исторический парк «Россия – моя История»"}
+        />
+        <Partner
+          url={"https://www.permgaspi.ru/"}
+          src={imgGaspi}
+          title={"Пермский государственных архив социально-политической истории"}
+        />
       </ul>
     </div>
   );
